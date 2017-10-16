2ede191ca6721536e73f04876d975896c56cfe26
Jonian Guveli <jonian@hardpixel.eu>
2017-10-16 11:08:25 +0300

    add schema cache

---
+++ lib/activerecord_tableless/schema_cache.rb
@@ -0,0 +1,15 @@
+module ActiverecordTableless
+  class SchemaCache
+    def columns_hash(table_name)
+      {}
+    end
+
+    def data_source_exists?(table_name)
+      false
+    end
+
+    def clear_data_source_cache!(table_name)
+      true
+    end
+  end
+end
